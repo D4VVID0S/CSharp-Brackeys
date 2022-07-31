01de225ca47a88fc36d5b905f22be2f3e6490c84
D4VVID0S <matix09@live.it>
2022-07-31 19:44:59 +0200

    Methods episode completed

--- Program.cs
+++ Program.cs
@@ -1,11 +1,1 @@
-﻿PrintNumber();
-
-void PrintNumber()
-{
-    Random numGen = new Random();
-    int number = numGen.Next(1, 10);
-    Console.WriteLine(number);
-}
-
-// Wait before closing
-Console.ReadKey();
+﻿
