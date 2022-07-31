fd1059b451a418a5e6c974226133141a62ef137c
D4VVID0S <matix09@live.it>
2022-07-31 19:05:03 +0200

    Began Methods

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿PrintNumber();
+
+void PrintNumber()
+{
+    Random numGen = new Random();
+    int number = numGen.Next(1, 10);
+    Console.WriteLine(number);
+}
+
+// Wait before closing
+Console.ReadKey();
