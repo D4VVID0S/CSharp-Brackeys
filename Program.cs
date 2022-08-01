bbfd270fd27a27a1e47263b05e04ee6c8785bc93
Matteo Catalani <matix09@live.it>
2022-08-01 02:21:15 +0200

    Created more wizards

--- Program.cs
+++ Program.cs
@@ -6,30 +6,54 @@ namespace CSharp_Brackeys_2
     {
         public string name = "default";
         public string favoriteSpell = "default";
-        public int spellSlots;
-        public float experience;
+        private int spellSlots;
+        private float experience; //removing the private/public word sets it automatically to provate
+
+        public static int Count;
+
+        // budowanie konstruktora
+        public Wizard(string _name, string _favoriteSpell)
+        {
+            name = _name;
+            favoriteSpell = _favoriteSpell;
+            spellSlots = 2;
+            experience = 0;
+
+            Count ++;
+        }
 
         public void CastSpell()
         {
-            Console.WriteLine(name + " casts " + favoriteSpell);
-            spellSlots--;
-            Console.WriteLine("Spellslots: " + spellSlots);
+            if (spellSlots > 0)
+            {
+                Console.WriteLine(name + " casts " + favoriteSpell);
+                spellSlots--;
+                experience = experience + 3;
+            }
+            else
+            {
+                Console.WriteLine("You are out of spell slots.");
+                Console.WriteLine("Spellslots: " + spellSlots);
+            }
+        }
+
+        public void Meditate()
+        {
+            Console.WriteLine(name + " meditates to regain spell slots.");
+            spellSlots = 2;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Wizard wizard01 = new Wizard();
-
-            wizard01.name = "Parry Hotter";
-            wizard01.favoriteSpell = "Unexpecto Patronum";
-            wizard01.spellSlots = 2;
-            wizard01.experience = 0f;
-
-
-            Console.WriteLine("Spellslots: " + wizard01.spellSlots);
+            Wizard wizard01 = new Wizard("Parry Hopper", "Unexpecto Patronum");
             wizard01.CastSpell();
+
+            Wizard wizard02 = new Wizard("Glindalf Merlinson", "Abracadabra");
+            wizard02.CastSpell();
+
+            Console.WriteLine("Created wizards: " + Wizard.Count);
 
             // Wait before closing
             Console.ReadKey();
