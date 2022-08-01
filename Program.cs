c02674433e797da8d22fb02e24c1e53c756b311c
Matteo Catalani <matix09@live.it>
2022-08-01 02:03:11 +0200

    Adjusted to basic (older) C# coding

--- Program.cs
+++ Program.cs
@@ -1,29 +1,38 @@
-﻿class Wizard
-{
-    public string name;
-    public string favoriteSpell;
-    public int spellSlots;
-    public float experience;
+﻿using System;
 
-    public void CastSpell()
+namespace CSharp_Brackeys_2
+{
+    class Wizard
     {
-        Console.WriteLine(name + " casts " + favoriteSpell);
-        spellSlots--;
+        public string name = "default";
+        public string favoriteSpell = "default";
+        public int spellSlots;
+        public float experience;
+
+        public void CastSpell()
+        {
+            Console.WriteLine(name + " casts " + favoriteSpell);
+            spellSlots--;
+            Console.WriteLine("Spellslots: " + spellSlots);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wizard wizard01 = new Wizard();
+
+            wizard01.name = "Parry Hotter";
+            wizard01.favoriteSpell = "Unexpecto Patronum";
+            wizard01.spellSlots = 2;
+            wizard01.experience = 0f;
+
+
+            Console.WriteLine("Spellslots: " + wizard01.spellSlots);
+            wizard01.CastSpell();
+
+            // Wait before closing
+            Console.ReadKey();
+        }
     }
 }
-
-// Main
-
-Wizard wizard01 = new Wizard();
-
-wizard01.name = "Parry Hopper";
-wizard01.favoriteSpell = "Unexpecto patronum";
-wizard01.spellSlots = 2;
-wizard01.experience = 0f;
-
-
-Console.WriteLine("Spellslots: " + wizard01.spellSlots);
-wizard01.CastSpell();
-
-// Wait before closing
-Console.ReadKey();
